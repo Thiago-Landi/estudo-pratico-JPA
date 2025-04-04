513ef3e943692e298a0a3e868ee88bd4255561f9
Thiago-Landi <thiagolandi0902@gmail.com>
2025-04-04 12:41:56 -0300

    [security] Auth basic

---
+++ src/main/java/com/Thiago_landi/libraryapi/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.Thiago_landi.libraryapi.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    return http	            
+	            .csrf(AbstractHttpConfigurer::disable)// Desativa a proteção CSRF, útil para APIs REST
+	            .httpBasic(Customizer.withDefaults())// Habilita a autenticação básica (usuário/senha via pop-up do navegador ou em headers HTTP) 
+	            .authorizeHttpRequests(authorize -> {// Configura regras de autorização para as requisições HTTP
+	                authorize.anyRequest().authenticated();// Exige autenticação para qualquer requisição na aplicação
+	            })
+	            .build();// Constrói e retorna a configuração de segurança
+	}
+}
